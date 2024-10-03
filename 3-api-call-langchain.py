9f47b086c18dce1e7af0c683aaf3e939ff22da0a
Seki Tomoya <sstomoya34@gmail.com>
2024-10-03 17:02:35 +0900

    パラメータを指定できるように修正

--- 3-api-call-langchain.py
+++ 3-api-call-langchain.py
@@ -1,7 +1,14 @@
 from langchain_aws import ChatBedrock
 from langchain_core.messages import HumanMessage
 
-chat = ChatBedrock(model_id="anthropic.claude-3-5-sonnet-20240620-v1:0")
+chat = ChatBedrock(
+    model_id = "anthropic.claude-3-5-sonnet-20240620-v1:0",
+    model_kwargs = {
+        "max_tokens": 1000,
+        "top_p": 0.9,
+        "temperature": 0.7,
+    }
+)
 
 messages = [HumanMessage(content="秋の俳句を詠んで")]
 
