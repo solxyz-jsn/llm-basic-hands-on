d1c60e9e2d5ec67eb2efe7c3f31b8be328352a83
Seki Tomoya <sstomoya34@gmail.com>
2024-10-09 15:17:41 +0900

    可読性向上のため半角スペースの追加

--- 6-1-rag-langchain-db-embedding.py
+++ 6-1-rag-langchain-db-embedding.py
@@ -12,24 +12,25 @@ def get_pdf_files(directory=".", specific_files=None):
     else:
         return [f for f in os.listdir(directory) if f.endswith('.pdf')]
 
-def process_pdfs_to_chroma(pdf_files, collection_name="pdf_embeddings", persist_directory="./chroma_db"):
+# PDFファイルを処理しChromaのベクトルデータベースに格納
+def process_pdfs_to_chroma(pdf_files, collection_name = "pdf_embeddings", persist_directory = "./chroma_db"):
     # Amazon Bedrock clientの設定
     bedrock_client = boto3.client(
-        service_name='bedrock-runtime',
-        region_name='us-west-2'  
+        service_name = 'bedrock-runtime',
+        region_name = 'us-west-2'  
     )
 
     # Bedrock Embeddingsの設定
     embeddings = BedrockEmbeddings(
-        client=bedrock_client,
-        model_id="cohere.embed-multilingual-v3"
+        client = bedrock_client,
+        model_id = "cohere.embed-multilingual-v3"
     )
 
     # Chromaベクトルストアの初期化（ローカルに保存）
-    vectorstore = Chroma(collection_name=collection_name, embedding_function=embeddings, persist_directory=persist_directory)
+    vectorstore = Chroma(collection_name = collection_name, embedding_function = embeddings, persist_directory = persist_directory)
 
     # テキスト分割の設定
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
 
     # 指定されたPDFファイルをVectorDBに登録する
     for pdf_file in pdf_files:
@@ -50,11 +51,10 @@ if __name__ == "__main__":
     specific_pdf_files = ["generativeAI-guideline-solxyz.pdf"]  
     
     # 指定されたPDFファイル名のリストを取得（引数がない場合は、ディレクトリ内のすべてを探索する）
-    pdf_files = get_pdf_files(specific_files=specific_pdf_files)
+    pdf_files = get_pdf_files(specific_files = specific_pdf_files)
     if not pdf_files:
         print("指定されたファイルが見つかりませんでした")
     else:
         print(f"{len(pdf_files)}件指定されたPDFファイルが見つかりました\nファイル名: {pdf_files}")
         process_pdfs_to_chroma(pdf_files)
         print(f"VectorDBの作成が完了し、'./chroma_db'ディレクトリ配下に保存されました")
-
